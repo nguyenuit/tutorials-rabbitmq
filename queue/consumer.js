0ad8cf8bd0c07c5f26ce221a917a08862b19ef65
nguyen <nguyen@digital-launch.com>
2022-12-27 10:43:01 +0700

    update producer, consumer, use noAck, TTL, durable, persistent

--- queue/consumer.js
+++ queue/consumer.js
@@ -1,27 +1,28 @@
 const amqplib = require('amqplib')
 
 const amqp_url_cloud = 'amqps://zckikibk:wb2U_HzitlYu4dnk92C6TWSbSUN4hgew@armadillo.rmq.cloudamqp.com/zckikibk'
+const amqp_url_docker = 'amqp://localhost:5672'
 
 const receiveQueue = async () => {
     try {
         //1. create connection
-        const conn = await amqplib.connect(amqp_url_cloud)
+        const conn = await amqplib.connect(amqp_url_docker)
         //2. create channel
         const channel = await conn.createChannel()
         //3. create queue name
         const queueName = 'q1'
         //4. create queue
         await channel.assertQueue(queueName, {
-            durable: false,
+            durable: true,
         })
         //5. receive to queue
         await channel.consume(queueName, msg => {
             console.log('Msg:', msg.content.toString())
         },{
-            noAck: false
+            noAck: true
         })
 
-        console.log('It\'s working!');
+        // console.log('It\'s working!');
         //6. close connection and channel
 
     } catch (error) {
--- queue/producer.js
+++ queue/producer.js
@@ -1,23 +1,27 @@
 const amqplib = require('amqplib')
 
 const amqp_url_cloud = 'amqps://zckikibk:wb2U_HzitlYu4dnk92C6TWSbSUN4hgew@armadillo.rmq.cloudamqp.com/zckikibk'
+const amqp_url_docker = 'amqp://localhost:5672'
 
 const sendQueue = async ({ msg }) => {
     try {
         //1. create connection
-        const conn = await amqplib.connect(amqp_url_cloud)
+        const conn = await amqplib.connect(amqp_url_docker)
         //2. create channel
         const channel = await conn.createChannel()
         //3. create queue name
         const queueName = 'q1'
         //4. create queue
         await channel.assertQueue(queueName, {
-            durable: false,
+            durable: true,
         })
         //5. send to queue
-        await channel.sendToQueue(queueName, Buffer.from(msg))
+        await channel.sendToQueue(queueName, Buffer.from(msg), {
+            persistent: true,
+            // expiration: '10000'
+        })
 
-        console.log('It\'s working!');
+        // console.log('It\'s working!');
         //6. close connection and channel
 
     } catch (error) {
@@ -25,4 +29,6 @@ const sendQueue = async ({ msg }) => {
     }
 }
 
-sendQueue({msg: 'Luong 3k! Minh da lam duoc!'})
+const msg = process.argv.slice(2).join(' ') || 'Hello';
+
+sendQueue({msg})
